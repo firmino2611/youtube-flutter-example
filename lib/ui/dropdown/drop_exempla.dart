de18acbb4a9338117f22c92aeabefc240a24ca91
Lucas Firmino <lucasfirmino@esquilodigital.com.br>
2020-11-05 15:46:23 -0300

    update

--- lib/ui/dropdown/drop_exempla.dart
+++ lib/ui/dropdown/drop_exempla.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class DropExample extends StatefulWidget {
   @override
@@ -6,15 +9,55 @@ class DropExample extends StatefulWidget {
 }
 
 class _DropExampleState extends State<DropExample> {
-  int _value = 1;
+  String _value = 'RO';
 
   List<Map<String, dynamic>> estados = [
-    {'nome': 'SP', 'id': 1},
-    {'nome': 'RJ', 'id': 2},
-    {'nome': 'MG', 'id': 3},
-    {'nome': 'ES', 'id': 4}
+    {'sigla': '', 'id': 0}
   ];
 
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_value.toString()),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: FutureBuilder(
+            future: http.get(
+                'https://servicodados.ibge.gov.br/api/v1/localidades/estados'),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List response = jsonDecode(snapshot.data.body);
+
+                return DropdownButton(
+                  value: _value,
+                  items: response
+                      .map((e) => DropdownMenuItem(
+                            child: Text(e['nome']),
+                            value: e['sigla'],
+                          ))
+                      .toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _value = value;
+                    });
+                  },
+                );
+              }
+
+              return Container();
+            }),
+      ),
+    );
+  }
+
+  // outra forma de fazer
   gerarListaEstados() {
     List<DropdownMenuItem<int>> auxiliar = [];
 
@@ -26,34 +69,5 @@ class _DropExampleState extends State<DropExample> {
     });
 
     return auxiliar;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_value.toString()),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        child: DropdownButton(
-          value: _value,
-          // items: gerarListaEstados(),
-          items: estados
-              .map(
-                (estado) => DropdownMenuItem(
-                  child: Text(estado['nome']),
-                  value: estado['id'],
-                ),
-              )
-              .toList(),
-          onChanged: (value) {
-            setState(() {
-              _value = value;
-            });
-          },
-        ),
-      ),
-    );
   }
 }
